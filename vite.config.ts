063fbe465a7801c9fa43d71b774d935e7fcb76d7
baozicn <163871355+baozicn@users.noreply.github.com>
2026-03-03 22:28:47 -0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({mode}) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
-      base: process.env.VITE_BASE || '/',
+      base:  process.env.NODE_ENV === 'production' ? '/aeroponic-web/' : '/',
   },
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
